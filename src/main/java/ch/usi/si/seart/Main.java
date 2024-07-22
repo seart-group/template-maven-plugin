d2d2a4ffe9c0ce58d3c4c1d68023748ac26fbd68
Ozren Dabić <dabico@usi.ch>
2024-07-22 09:37:44 +0200

    Initial commit

---
+++ src/main/java/ch/usi/si/seart/Main.java
@@ -0,0 +1,8 @@
+package ch.usi.si.seart;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.exit(0);
+    }
+}
---
+++ src/test/java/ch/usi/si/seart/MainTest.java
@@ -0,0 +1,12 @@
+package ch.usi.si.seart;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Assertions.assertTrue(true);
+    }
+}
